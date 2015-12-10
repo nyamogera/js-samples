9d03bb5ae5be4ee1c83c0f4e1c77f53ef436902a
nyamogera <nyamogera@gmail.com>
2015-12-10 15:02:00 +0900

    ネガポジ反転コメント追加

--- 151210_postprocessing-negative-positive/main.js
+++ 151210_postprocessing-negative-positive/main.js
@@ -1,13 +1,15 @@
 window.onload = function () {
-    init();
-    animate();
+  init();
+  animate();
 };
+
 
 var camera, scene, renderer, composer;
 var object, light;
 
+// /////////////////////////////////////シェーダー部分///始まり
 const uniforms = {
-    "tDiffuse": { type: "t", value: null }
+  "tDiffuse": { type: "t", value: null }
 };
 
 const vertexShader = `
@@ -26,82 +28,84 @@ const fragmentShader = `
 varying vec2 vUv;
 uniform sampler2D tDiffuse;
 void main() {
-    vec4 color = texture2D(tDiffuse, vUv);
-    gl_FragColor = vec4(1.0 - color.x, 1.0 - color.y, 1.0 - color.z, 1.0);
+  vec4 color = texture2D(tDiffuse, vUv);
+  gl_FragColor = vec4(1.0 - color.x, 1.0 - color.y, 1.0 - color.z, 1.0);
 }
 `
 
 var shader = {
-    uniforms:uniforms,
-    vertexShader:vertexShader,
-    fragmentShader:fragmentShader
+  uniforms: uniforms,
+  vertexShader: vertexShader,
+  fragmentShader: fragmentShader
 }
+
+// /////////////////////////////////////シェーダー部分///終わり
 
 function init() {
 
-    renderer = new THREE.WebGLRenderer();
-    renderer.setPixelRatio(window.devicePixelRatio);
-    renderer.setSize(window.innerWidth, window.innerHeight);
-    document.body.appendChild(renderer.domElement);
+  renderer = new THREE.WebGLRenderer();
+  renderer.setPixelRatio(window.devicePixelRatio);
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  document.body.appendChild(renderer.domElement);
 
-    //
+  //
 
-    camera = new THREE.PerspectiveCamera(70, window.innerWidth / window.innerHeight, 1, 1000);
-    camera.position.z = 400;
+  camera = new THREE.PerspectiveCamera(70, window.innerWidth / window.innerHeight, 1, 1000);
+  camera.position.z = 400;
 
-    scene = new THREE.Scene();
-    scene.fog = new THREE.Fog(0x000000, 1, 1000);
+  scene = new THREE.Scene();
+  scene.fog = new THREE.Fog(0x000000, 1, 1000);
 
-    object = new THREE.Object3D();
-    scene.add(object);
+  object = new THREE.Object3D();
+  scene.add(object);
 
-    var geometry = new THREE.SphereGeometry(1, 4, 4);
-    var material = new THREE.MeshPhongMaterial({ color: 0xffffff, shading: THREE.FlatShading });
+  var geometry = new THREE.SphereGeometry(1, 4, 4);
+  var material = new THREE.MeshPhongMaterial({ color: 0xffffff, shading: THREE.FlatShading });
 
-    for (var i = 0; i < 100; i++) {
+  for (var i = 0; i < 100; i++) {
 
-        var mesh = new THREE.Mesh(geometry, material);
-        mesh.position.set(Math.random() - 0.5, Math.random() - 0.5, Math.random() - 0.5).normalize();
-        mesh.position.multiplyScalar(Math.random() * 400);
-        mesh.rotation.set(Math.random() * 2, Math.random() * 2, Math.random() * 2);
-        mesh.scale.x = mesh.scale.y = mesh.scale.z = Math.random() * 50;
-        object.add(mesh);
+    var mesh = new THREE.Mesh(geometry, material);
+    mesh.position.set(Math.random() - 0.5, Math.random() - 0.5, Math.random() - 0.5).normalize();
+    mesh.position.multiplyScalar(Math.random() * 400);
+    mesh.rotation.set(Math.random() * 2, Math.random() * 2, Math.random() * 2);
+    mesh.scale.x = mesh.scale.y = mesh.scale.z = Math.random() * 50;
+    object.add(mesh);
 
-    }
+  }
 
-    scene.add(new THREE.AmbientLight(0x222222));
-    light = new THREE.DirectionalLight(0xffffff);
-    light.position.set(1, 1, 1);
-    scene.add(light);
+  scene.add(new THREE.AmbientLight(0x222222));
+  light = new THREE.DirectionalLight(0xffffff);
+  light.position.set(1, 1, 1);
+  scene.add(light);
 
-    // postprocessing
-    composer = new THREE.EffectComposer(renderer);
-    composer.addPass(new THREE.RenderPass(scene, camera));
+  // postprocessing
+  composer = new THREE.EffectComposer(renderer);
+  composer.addPass(new THREE.RenderPass(scene, camera));
 
-    var pass = new THREE.ShaderPass(shader);
-    composer.addPass(pass);
-    pass.renderToScreen = true;
-    pass.enabled = true;
+  var pass = new THREE.ShaderPass(shader);
+  composer.addPass(pass);
+  pass.renderToScreen = true;
+  pass.enabled = true;
 
-    window.addEventListener('resize', onWindowResize, false);
+  window.addEventListener('resize', onWindowResize, false);
 
 }
 
 function onWindowResize() {
 
-    camera.aspect = window.innerWidth / window.innerHeight;
-    camera.updateProjectionMatrix();
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
 
-    renderer.setSize(window.innerWidth, window.innerHeight);
+  renderer.setSize(window.innerWidth, window.innerHeight);
 
 }
 
 function animate() {
-    requestAnimationFrame(animate);
+  requestAnimationFrame(animate);
 
-    object.rotation.x += 0.005;
-    object.rotation.y += 0.01;
+  object.rotation.x += 0.005;
+  object.rotation.y += 0.01;
 
-    composer.render();
-    //renderer.render(scene, camera);
+  composer.render();
+  //renderer.render(scene, camera);
 }
